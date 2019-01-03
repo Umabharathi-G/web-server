82f718b8a034ffd045c59ee88e1fa46714a4ea5d
unknown <umabharathi2203@gmail.com>
2019-01-03 20:13:02 +0530

    Setup start script and heroku

--- server.js
+++ server.js
@@ -2,58 +2,55 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
-
-app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-	var now = new Date().toString();
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-	var log = `${now}: ${req.method} ${req.url}`;
-	console.log(log);
-	fs.appendFile('server.log',log + '\n', (err) => {
-		if(err){
-			console.log('unable to append to server.log');
-		}
-	});
-	next();
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
-/*app.use((req, res, next) => {
-	res.render('maintain.hbs', {
-	});
-});
-*/
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
-	return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper('upperCase', (text) => {
-	return text.toUpperCase();
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
-app.get('/',(req, res) => {
-	res.render('home.hbs',{
-		pageTitle: 'Home Page',
-		welcome: 'Welcome to the Home Page'
-	});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
-
-app.get('/about',(req,res) => {
-	res.render('about.hbs',{
-		pageTitle: 'About Page',
-	});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
-app.get('/bad', (req,res) => {
-	res.send({
-		msg: 'unable to handle request'
-	});
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
-app.listen(8000, () => {
-	console.log('Server is up and running on port 8000');
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
